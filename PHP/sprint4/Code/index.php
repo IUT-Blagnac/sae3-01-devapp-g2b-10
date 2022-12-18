db653e8b9aa7a23cee24de1bfdc58925bcb5e9ef
☆ Mazlai <57178136+Mazlai@users.noreply.github.com>
2022-12-18 21:04:16 +0100

    Ajout du code PHP, HTML & CSS de la semaine

---
+++ PHP/sprint4/Code/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+    <link rel="stylesheet" href="./include/styles.css">
+	<title>Art'i'Chaude</title>
+</head>
+<body>
+	<?php include("./include/header.php"); ?>
+ 
+    <div style="padding-top: 30px" id="main">
+        <div style="text-align: center" class="col-md-12">
+
+        </div>
+    </div>
+
+	<?php include("./include/footer.php"); ?>
+</body>
+</html>

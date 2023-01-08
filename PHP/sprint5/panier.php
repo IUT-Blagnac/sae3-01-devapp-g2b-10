ded7db31ed8987d3d81ea67ad0d914e04f4d506e
Mickael Fernandez <57178136+Mazlai@users.noreply.github.com>
2023-01-08 20:27:35 +0100

    Ajout des fichiers relatifs à la partie PHP.

---
+++ PHP/sprint5/panier.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+    <link rel="stylesheet" href="./include/styles.css"/>
+	<title>Produit</title>
+</head>
+<body>
+	<?php include("./include/header.php");
+      ?>
+    <div class="squarE">
+
+
+<div id="titleFrame"><h1 id="formTitle">Panier :</h1></div>
+
+
+
+
+
+
+
+</div>
+      <?php include("./include/footer.php"); ?>
+  </body>
+</html>
